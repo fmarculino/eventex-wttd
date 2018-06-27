55c58e915b67f8e4ef4f9ed991dbc28cd25e4c42
fernando <fernandomarculino@gmail.com>
2018-06-27 13:37:06 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Fernando Marculino',
+            cpf='53638930459',
+            email='fernandomarculino@gmail.com',
+            phone='94-991894670'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at, attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,8 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+
 
 class SubscribeGet(TestCase):
     def setUp(self):
@@ -12,7 +14,8 @@ class SubscribeGet(TestCase):
 
     def test_template(self):
         """Must use subscriptions/subscription_form.html"""
-        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+        self.assertTemplateUsed(self.resp,
+                                'subscriptions/subscription_form.html')
 
     def test_html(self):
         """Html must contain input tags"""
@@ -41,12 +44,16 @@ class SubscribePostValid(TestCase):
         data = dict(name='Fernando Marculino', cpf='55544433322',
                     email='fernandomarculino@gmail.com', phone='94-99189-4670')
         self.resp = self.client.post('/inscricao/', data)
+
     def test_post(self):
         """Valid POST should redirect to /inscricao/"""
         self.assertEqual(302, self.resp.status_code)
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
+
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 
 class SubscribePostInvalid(TestCase):
@@ -58,7 +65,8 @@ class SubscribePostInvalid(TestCase):
         self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
-        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+        self.assertTemplateUsed(self.resp,
+                                'subscriptions/subscription_form.html')
 
     def test_has_form(self):
         form = self.resp.context['form']
@@ -67,6 +75,9 @@ class SubscribePostInvalid(TestCase):
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessage(TestCase):
